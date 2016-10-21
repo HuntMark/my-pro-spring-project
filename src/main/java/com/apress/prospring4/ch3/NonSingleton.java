1a2dac52106e9d61035e6b305d60897e06c8281f
HuntMark <rinat.zalyaletdinov@yandex.com>
2016-10-21 10:45:50 +0300

    Understanding Bean Instantiation Mode

---
+++ src/main/java/com/apress/prospring4/ch3/NonSingleton.java
@@ -0,0 +1,19 @@
+package com.apress.prospring4.ch3;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class NonSingleton {
+  public static void main(String[] args) {
+    GenericXmlApplicationContext context = new GenericXmlApplicationContext();
+    context.load("classpath:app-context-xml.xml");
+    context.refresh();
+
+    String s1 = (String) context.getBean("nonSingleton");
+    String s2 = (String) context.getBean("nonSingleton");
+
+    System.out.println("Identity Equal?: " + (s1 == s2));
+    System.out.println("Value Equal?: " + s1.equals(s2));
+    System.out.println(s1);
+    System.out.println(s2);
+  }
+}
