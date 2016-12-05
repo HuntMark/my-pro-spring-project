a701cce83f0cead221b2358b898e168578b32d06
HuntMark <rinat.zalyaletdinov@yandex.com>
2016-12-05 22:10:25 +0300

    Configuration Using Java Classes

--- src/main/java/com/apress/prospring4/ch4/AppConfig.java
+++ src/main/java/com/apress/prospring4/ch4/AppConfig.java
@@ -1,16 +1,28 @@
 package com.apress.prospring4.ch4;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@ImportResource(value = "classpath:app-context-xml.xml")
+@PropertySource(value = "classpath:message.properties")
+@ComponentScan(basePackages = {"com.apress.prospring4.ch4"})
+@EnableTransactionManagement
 public class AppConfig {
-    @Bean
-    public MessageProvider messageProvider() {
-        return new ConfigurableMessageProvider();
-    }
+    @Autowired
+    Environment env;
 
     @Bean
+    @Lazy(value = true)
+    public MessageProvider messageProvider() {
+        return new ConfigurableMessageProvider(env.getProperty("message"));
+    }
+
+    @Bean(name = "messageRenderer")
+    @Scope(value = "prototype")
+    @DependsOn(value = "messageProvider")
     public MessageRenderer messageRenderer() {
         StandardOutMessageRenderer renderer = new StandardOutMessageRenderer();
         renderer.setMessageProvider(messageProvider());
