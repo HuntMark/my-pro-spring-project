c13ea923493e856ac339ce6edcaaad4f34f82430
Rinat Zalyaletdinov <rinat_zalyaletdinov@epam.com>
2018-02-12 09:47:30 +0300

    Chapter 6: Retrieving a Single-Value by Using JdbcTemplate

---
+++ src/main/java/com/apress/prospring4/ch6/JdbcContactDao.java
@@ -0,0 +1,67 @@
+package com.apress.prospring4.ch6;
+
+import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+public class JdbcContactDao implements ContactDao, InitializingBean {
+    private DataSource dataSource;
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public List<Contact> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<Contact> findByFirstName(String firstName) {
+        return null;
+    }
+
+    @Override
+    public String findLastNameById(Long id) {
+        return null;
+    }
+
+    @Override
+    public String findFirstNameById(Long id) {
+        return jdbcTemplate.queryForObject("SELECT first_name FROM contact WHERE id = ?",
+                new Object[]{id}, String.class);
+    }
+
+    @Override
+    public void insert(Contact contact) {
+    }
+
+    @Override
+    public void update(Contact contact) {
+    }
+
+    @Override
+    public void delete(Long contactId) {
+    }
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.setDataSource(dataSource);
+        MySQLErrorCodesTranslator errorTranslator =
+                new MySQLErrorCodesTranslator();
+        errorTranslator.setDataSource(dataSource);
+        jdbcTemplate.setExceptionTranslator(errorTranslator);
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        if (dataSource == null) {
+            throw new BeanCreationException("Must set dataSource on ContactDao");
+        }
+        if (jdbcTemplate == null) {
+            throw new BeanCreationException("Null JdbcTemplate on ContactDao");
+        }
+    }
+}
