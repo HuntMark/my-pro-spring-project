628b93516e00be013777738d9e11dc999909fa35
Rinat Zalyaletdinov <rinat_zalyaletdinov@epam.com>
2018-03-06 09:40:32 +0300

    Chapter 8: Using Spring Data JPA Repository Abstraction for Database Operations

--- src/main/java/com/apress/prospring4/ch8/ContactServiceImpl.java
+++ src/main/java/com/apress/prospring4/ch8/ContactServiceImpl.java
@@ -1,103 +1,37 @@
 package com.apress.prospring4.ch8;
 
-import static org.slf4j.LoggerFactory.getLogger;
-
-import org.slf4j.Logger;
+import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.JoinType;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
 import java.util.List;
 
-@Service("jpaContactService")
+@Service("springJpaContactService")
 @Repository
 @Transactional
 public class ContactServiceImpl implements ContactService {
 
-    private static final Logger log = getLogger(ContactServiceImpl.class);
+    private ContactRepository contactRepository;
 
-    final static String ALL_CONTACT_NATIVE_QUERY =
-            "select id, first_name, last_name, birth_date, version from contact";
-
-    @PersistenceContext
-    private EntityManager em;
+    @Autowired
+    public void setContactRepository(ContactRepository contactRepository) {
+        this.contactRepository = contactRepository;
+    }
 
     @Transactional(readOnly = true)
-    @Override
     public List<Contact> findAll() {
-        return em.createNamedQuery("Contact.findAll", Contact.class)
-                .getResultList();
+        return Lists.newArrayList(contactRepository.findAll());
     }
 
     @Transactional(readOnly = true)
-    @Override
-    public List<Contact> findAllWithDetail() {
-        return em.createNamedQuery("Contact.findAllWithDetail", Contact.class)
-                .getResultList();
+    public List<Contact> findByFirstName(String firstName) {
+        return contactRepository.findByFirstName(firstName);
     }
 
     @Transactional(readOnly = true)
-    @Override
-    public Contact findById(Long id) {
-        return em.createNamedQuery("Contact.findById", Contact.class)
-                .setParameter("id", id)
-                .getSingleResult();
-    }
-
-    @Override
-    public Contact save(Contact contact) {
-        if (contact.getId() == null) {
-            log.info("Inserting new contact");
-            em.persist(contact);
-        } else {
-            em.merge(contact);
-            log.info("Updating existing contact");
-        }
-        log.info("Contact saved with id: " + contact.getId());
-        return contact;
-    }
-
-    @Override
-    public void delete(Contact contact) {
-        Contact mergedContact = em.merge(contact);
-        em.remove(mergedContact);
-        log.info("Contact with id: " + contact.getId() + " deleted successfully");
-    }
-
-    @Transactional(readOnly = true)
-    @Override
-    public List<Contact> findAllByNativeQuery() {
-        return em.createNativeQuery(ALL_CONTACT_NATIVE_QUERY,
-                "contactResult").getResultList();
-    }
-
-    @Transactional(readOnly = true)
-    @Override
-    public List<Contact> findByCriteriaQuery(String firstName, String lastName) {
-        log.info("Finding contact for firstName: " + firstName + " and lastName: " + lastName);
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<Contact> criteriaQuery = cb.createQuery(Contact.class);
-        Root<Contact> contactRoot = criteriaQuery.from(Contact.class);
-        contactRoot.fetch(Contact_.contactTelDetails, JoinType.LEFT);
-        contactRoot.fetch(Contact_.hobbies, JoinType.LEFT);
-        criteriaQuery.select(contactRoot).distinct(true);
-        Predicate criteria = cb.conjunction();
-        if (firstName != null) {
-            Predicate p = cb.equal(contactRoot.get(Contact_.firstName), firstName);
-            criteria = cb.and(criteria, p);
-        }
-        if (lastName != null) {
-            Predicate p = cb.equal(contactRoot.get(Contact_.lastName), lastName);
-            criteria = cb.and(criteria, p);
-        }
-        criteriaQuery.where(criteria);
-        return em.createQuery(criteriaQuery).getResultList();
+    public List<Contact> findByFirstNameAndLastName(String firstName, String lastName) {
+        return contactRepository.findByFirstNameAndLastName(firstName, lastName);
     }
 }
--- src/main/java/com/apress/prospring4/ch8/SpringJpaSample.java
+++ src/main/java/com/apress/prospring4/ch8/SpringJpaSample.java
@@ -5,42 +5,23 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 import java.util.List;
 
 public class SpringJpaSample {
-
     public static void main(String[] args) {
         GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
         ctx.load("classpath:app-context-jpa.xml");
         ctx.refresh();
-
-        ContactService contactService = ctx.getBean("jpaContactService", ContactService.class);
-        List<Contact> contacts = contactService.findByCriteriaQuery("John", "Smith");
-        listContactsWithDetail(contacts);
+        ContactService contactService = ctx.getBean(
+                "springJpaContactService", ContactService.class);
+        listContacts("Find all:", contactService.findAll());
+        listContacts("Find by first name:", contactService.findByFirstName("Chris"));
+        listContacts("Find by first and last name:",
+                contactService.findByFirstNameAndLastName("Chris", "Schaefer"));
     }
 
-    private static void listContacts(List<Contact> contacts) {
+    private static void listContacts(String message, List<Contact> contacts) {
         System.out.println("");
-        System.out.println("Listing contacts without details:");
+        System.out.println(message);
         for (Contact contact : contacts) {
             System.out.println(contact);
-            System.out.println();
-        }
-    }
-
-    private static void listContactsWithDetail(List<Contact> contacts) {
-        System.out.println("");
-        System.out.println("Listing contacts with details:");
-        for (Contact contact : contacts) {
-            System.out.println(contact);
-            if (contact.getContactTelDetails() != null) {
-                for (ContactTelDetail contactTelDetail :
-                        contact.getContactTelDetails()) {
-                    System.out.println(contactTelDetail);
-                }
-            }
-            if (contact.getHobbies() != null) {
-                for (Hobby hobby : contact.getHobbies()) {
-                    System.out.println(hobby);
-                }
-            }
             System.out.println();
         }
     }
